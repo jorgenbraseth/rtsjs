ba8df717c350099cd187e2aac22c05ec766ea221
Jørgen Braseth <jorgen.braseth@gmail.com>
2016-01-19 01:06:11 +0100

    Heroku stuff

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname+"/build/")).listen(80);
+connect().use(serveStatic(__dirname+"/build/")).listen(8080);
