1d130a5637fa5f22c5bd58db6e79d02f9318f0fc
Jørgen Braseth <jorgen.braseth@gmail.com>
2016-03-23 23:14:52 +0100

    Fixed port config to work on heroku

--- server.js
+++ server.js
@@ -4,6 +4,5 @@ var serveStatic = require('serve-static');
 // when on Heroku, port will be exported to an environment variable
 // and available as process.env.PORT
 var port = process.env.PORT || CONFIG.port;
-app.listen(port);
 
 connect().use(serveStatic(__dirname+"/build/")).listen(port);
